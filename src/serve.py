578731b561ce767b6ab0c78a2570c104860f5ab6
fastium <fastium.pro@proton.me>
2025-12-13 21:30:05 +0100

    feat(CI/CD): allow serve pytorch model

--- src/serve.py
+++ src/serve.py
@@ -4,33 +4,39 @@ import json
 from typing import Annotated, Any, Dict, List
 
 import bentoml
+from bentoml.models import BentoModel
 import pandas as pd
 from bentoml.validators import ContentType
+
+import torch
+
+from evaluate import make_predictions
 
 
 @bentoml.service(name="air_temperature_regressor")
 class AirTemperatureRegressorService:
-    bento_model = bentoml.models.get("baseline:latest")
+    bento_model = BentoModel("baseline:latest")
 
     def __init__(self) -> None:
-        import tensorflow as tf
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model_path = self.bento_model.path_of("saved_model.pt")
+        self.model = torch.load(model_path,map_location=self.device)
+        print(f"Using device: {self.device}")
 
-        self.model = tf.keras.models.load_model(self.bento_model.path)
+        self.model.eval()
 
     @bentoml.api()
     def status(self) -> Annotated[str, ContentType("application/json")]:
         return json.dumps(
             {
                 "status": "OK",
-                "model": "air_temperature_regressor",
-                "version": str(self.bento_model.tag),
+                "model": str(self.bento_model.tag),
             }
         )
 
     @bentoml.api()
     def predict(self, data: Dict[str, List[Any]]) -> Dict[str, List[Any]]:
         input = pd.DataFrame(data)
-
         input["reference_timestamp"] = pd.to_datetime(input["reference_timestamp"])
 
         timestamp = input["reference_timestamp"].to_list()
@@ -39,13 +45,11 @@ class AirTemperatureRegressorService:
             ["reference_timestamp", "air_temperature", "historical"], axis=1
         )
 
-        predictions = self.model.predict(features)
+        predictions = make_predictions(self.model, features, self.device)
 
-        # calculate timestamp of predicted values shifted by 24 hours
         for i in range(len(timestamp)):
             timestamp.append(timestamp[i] + pd.Timedelta(hours=24))
 
-        # prepare all temperatures
         for i in range(len(predictions)):
             air_temperature.append(predictions[i])
 
